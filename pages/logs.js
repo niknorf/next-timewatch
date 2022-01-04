8851d18d769c885fa3191b920ae60ded64644f69
Simo Nurmi <simo.aleksi.nurmi@gmail.com>
2022-01-04 17:55:44 +0200

    Format with eslint, prettier

--- pages/logs.js
+++ pages/logs.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 import Layout from "../components/layout";
 
 export default function Logs() {
-	return (
-		<>
-			<Layout>
-				<Head>
-					<title>Logs</title>
-				</Head>
-				<h1>Logs</h1>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Logs</title>
+        </Head>
+        <h1>Logs</h1>
+      </Layout>
+    </>
+  );
 }
--- pages/stats.js
+++ pages/stats.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 import Layout from "../components/layout";
 
 export default function Stats() {
-	return (
-		<>
-			<Layout>
-				<Head>
-					<title>Stats</title>
-				</Head>
-				<h1>Stats</h1>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Stats</title>
+        </Head>
+        <h1>Stats</h1>
+      </Layout>
+    </>
+  );
 }
